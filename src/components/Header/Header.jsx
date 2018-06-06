cb9ceb67acae71814bec7d63f90739a34a21bb15
Lin Chen <lin.chen@innovation.hrs.com>
2018-06-06 16:31:22 +0200

    add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const conatinerFluid = {
+    paddingRight: "15px",
+    paddingLeft: "15px",
+    marginRight: "auto",
+    marginLeft: "auto",
+    width: "100%"
+};
+const container = {
+    ...conatinerFluid,
+    "@media (min-width: 576px)": {
+        maxWidth: "540px"
+    },
+    "@media (min-width: 768px)": {
+        maxWidth: "720px"
+    },
+    "@media (min-width: 992px)": {
+        maxWidth: "960px"
+    },
+    "@media (min-width: 1200px)": {
+        maxWidth: "1140px"
+    }
+};
+
+const styles = {
+    appBar: {
+        display: "flex",
+        border: "0",
+        borderRadius: "3px",
+        padding: "0.625rem 0",
+        marginBottom: "20px",
+        color: "#555",
+        width: "100%",
+        backgroundColor: "#fff",
+        boxShadow:
+            "0 4px 18px 0px rgba(0, 0, 0, 0.12), 0 7px 10px -5px rgba(0, 0, 0, 0.15)",
+        transition: "all 150ms ease 0s",
+        alignItems: "center",
+        flexFlow: "row nowrap",
+        justifyContent: "flex-start",
+        position: "relative"
+    },
+    transparent: {
+        backgroundColor: "red", //"transparent !important",
+        boxShadow: "none",
+        paddingTop: "25px",
+        color: "#FFFFFF"
+    },
+    container: {
+        ...container,
+        minHeight: "50px",
+        flex: "1",
+        alignItems: "center",
+        justifyContent: "space-between",
+        display: "flex",
+        flexWrap: "nowrap"
+    }
+};
+
+function Header(props) {
+    const { classes, color } = props;
+    const appBarClasses = classNames({
+        [classes.appBar]: true,
+        [classes[color]]: color
+    });
+    return (
+        <AppBar className={appBarClasses}>
+            <Toolbar className={classes.container}>Title</Toolbar>
+        </AppBar>
+    );
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
--- src/views/ProfilePage/ProfilePage.jsx
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "components/Header/Header";
 
-const ProfilePage = () => <div>profile page</div>;
+const ProfilePage = () => (
+    <div>
+        <Header color="transparent" />
+    </div>
+);
 
 export default ProfilePage;
